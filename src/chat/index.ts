a0027a7522828ddb67dec2070feb3452ff090e18
Dmitry Barykin <dmit.bar716@gmail.com>
2023-05-05 20:17:42 +0300

    Setup Package

---
+++ src/chat/index.ts
@@ -0,0 +1,16 @@
+interface IRoom {
+  uuid: string;
+  title: string;
+  description: string;
+  lastMessage?: Omit<IMessage, "roomUUID">;
+}
+
+interface IMessage {
+  uuid: string;
+  roomUUID: string;
+  authorID: string;
+  authorName: string;
+  message: string;
+}
+
+export { IRoom, IMessage };
---
+++ src/socket-events/messages.ts
@@ -0,0 +1,20 @@
+import { IRoom } from "../chat";
+import { SocketEventsEnum } from "./events";
+
+interface IGeneralEvent {
+  eventType: SocketEventsEnum;
+}
+
+interface IUserMeta {
+  login: string;
+}
+
+interface INotifyNewConnectionRq extends IGeneralEvent {
+  payload: IUserMeta;
+}
+
+interface INotifyNewConnectionRs {
+  payload: IRoom[];
+}
+
+export type { INotifyNewConnectionRq, INotifyNewConnectionRs };
